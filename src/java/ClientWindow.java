638b187acadf2287a7996782e062d37418c5f86a
Alex Romaniuk <alaster.rom@gmail.com>
2020-10-14 10:58:38 +0300

    add

--- src/java/ClientWindow.java
+++ src/java/ClientWindow.java
@@ -44,4 +44,14 @@ public class AddTwoNumbers {
         }
     }
 	
+	public static void main (String[] args) {
+		
+		double a = 3, b = 4;
+		int c;
+		
+		c = Math.sqrt (a* a + b* b);
+		
+		System.out.println ("c = "+ c);
+	}
+	
 }
