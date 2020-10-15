66d27d53e51aae8161ab3230e74eb32f8d2f0d89
Alex Romaniuk <alaster.rom@gmail.com>
2020-10-15 11:56:51 +0300

    add chenge

--- src/java/AddTwoNumbers.java
+++ src/java/AddTwoNumbers.java
@@ -4,9 +4,7 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5;
-	  int num2 = 15;
-	  int sum;
+      int num1 = 5, num2 = 15, sum;
 
       sum = num1 + num2;
       
@@ -15,19 +13,7 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5;
-	  int num2 = 15;
-	  int dif;
-
-      dif = num1 - num2;
-	  
-      logger.log("Sum of these numbers: " + dif);
-   }
-   
-   public static void main(String[] args) {
-      
-      int num1 = 5;
-	  int num2 = 15;
+      int num1 = 5, num2 = 15;
 	  int del;
 
       del = num1 / num2;
