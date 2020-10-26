3ac8bde7d7ab81b1e8a4bbb49ffdb5ab79ec354e
Alex Romaniuk <alaster.rom@gmail.com>
2020-10-26 09:11:52 +0200

    add

--- src/java/AddTwoNumbers.java
+++ src/java/AddTwoNumbers.java
@@ -3,11 +3,21 @@ package src;
 public class AddTwoNumbers {
 	
 	private int del;
+	private int reg;
    
    public static void main(String[] args) {
       
       int num1 = 5. num2 = 15;
 	  int sum;
+
+      sum = num1 + num2;
+      
+      logger.log("Sum of these numbers: " + sum);
+   }
+   
+   public static void main(String[] args) {
+      
+      int num1 = 5. num2 = 15, sum;
 
       sum = num1 + num2;
       
