ffc8368f82b7b978fcb333336378f45f7132cfcc
Alex Romaniuk <alaster.rom@gmail.com>
2020-10-26 14:59:51 +0200

    add

--- src/java/br1/ClientWindow.java
+++ src/java/br1/ClientWindow.java
@@ -33,6 +33,9 @@ public class AddTwoNumbers {
 	
 	public static void main(String[] args) {
       int num1 = 5, num2 = 15, dif;
+	  
+	  private int numl;
+	  
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }  
