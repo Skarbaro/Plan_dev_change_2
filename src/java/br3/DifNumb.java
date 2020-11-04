cd9705242a7ee10d32fabd46e2558ecdfe5c2d8d
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-04 10:04:15 +0200

    add

--- src/java/br3/DifNumb.java
+++ src/java/br3/DifNumb.java
@@ -1,7 +1,8 @@
 public class DifNumbers {
 
-   public static void main(String[] args) {
-      int num1 = 5, num2 = 15, dif;
+   public static void main(String args[]) {
+      int num1 = 5;
+	  int num2 = 15, dif;
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }
