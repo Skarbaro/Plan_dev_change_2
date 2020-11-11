4a0b468c8e77a2f8a3c794a161a7645515c2711d
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-11 09:19:11 +0200

    1 file

--- src/java/br3/Variab1.java
+++ src/java/br3/Variab1.java
@@ -2,11 +2,12 @@ package src.java;
 
 class Variab {
 	
-	public static void main (String[] args) {
+	public static void main (String args[]) {
 		
-		double a = 3;
-		int b = 4;
+		double a = 3, b = 4;
 		int c;
+		
+		private des;
 		
 		c = Math.sqrt (a* a + b* b);
 		
