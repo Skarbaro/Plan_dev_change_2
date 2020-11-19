a328e06bb796a6f9cbabc23eb67bae9e41ad6ac1
Alaster <alaster.raf@gmail.com>
2020-11-19 12:28:03 +0200

    add issues

--- src/java/Client.java
+++ src/java/Client.java
@@ -1,11 +1,13 @@
 package src;
 
 public class AddTwoNumbers {
+	
+	private int sre;
 
 	public static void main(String[] args) {
-		int num1 = 5;
-		int num2 = 15, sum;
+		int num1 = 5, num2 = 15, sum. dif;
 		sum = num1 / num2;
 		System.out.println("Sum of these numbers: " + sum);
+		System.out.println("Sum of these numbers: " + dif);
 	}
 }
