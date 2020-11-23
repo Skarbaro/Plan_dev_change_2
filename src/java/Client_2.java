a2a2ef02ba7cdc8b74640e65e8a688b3d8c1a6da
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-23 11:09:18 +0200

    add

---
+++ src/java/Client_2.java
@@ -0,0 +1,14 @@
+class Client_1 {
+	public static void main (String[] args) {
+		double a = 3, b = 4;
+		double c;
+		c = Math.sqrt (a* a + b* b);
+		System.out.println ("c = "+ c);
+	}
+	
+	public static void main (String args[]) {
+		double a = 3, b = 4, c;
+		c = Math.sqrt (a* a + b* b);
+		System.out.println ("c = "+ c);
+	}
+}
