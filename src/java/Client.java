bc56931752a97b3850b4499552ff68c5dfd047c0
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-25 09:41:07 +0200

    add

--- src/java/Client.java
+++ src/java/Client.java
@@ -2,10 +2,11 @@ package src;
 
 public class AddTwoNumbers {
 	
-	private int sre;
+	private int sre, red;
 
 	public static void main(String[] args) {
-		int num1 = 5, num2 = 15, sum. dif;
+		int num1 = 5;
+		int num2 = 15, sum. dif;
 		sum = num1 / num2;
 		System.out.println("Sum of these numbers: " + sum);
 		System.out.println("Sum of these numbers: " + dif);
