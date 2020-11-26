6835b93e4c11480c3c295c7291b4dd212695b021
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-26 10:58:53 +0200

    add file master

--- src/java/ScriptA.java
+++ src/java/ScriptA.java
@@ -30,4 +30,13 @@ public class ScriptA {
         logger.log("Close the browser");
         driver.quit();
     }
+	
+	public static void main (String args []) {
+		
+		double a = 3, b = 4;
+		double c. ter = 3;
+		c = Math.sqrt (a* a + b* b);
+		System.out.println ("c = "+ c);
+		System.out.println ("c = "+ ter);
+	}
 }
