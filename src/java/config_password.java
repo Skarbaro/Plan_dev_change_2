10bce23377478c28edd4ac54f22ca786952760d4
Alex Romaniuk <alaster.rom@gmail.com>
2020-12-02 18:50:58 +0200

    update file

--- src/java/config_password.java
+++ src/java/config_password.java
@@ -4,15 +4,6 @@ import infrastructure.config.utils.StringUtils;
 
 public class TestApp {
 	
-	int verifyAdmin(String password) {
-
-	if (password.equals("Mew!")) {
-		return 0;
-	}
-	//Diagnostic Mode
-	return 1;
-}
-	
 	private int selv;
 	
 	public static void main(String args[]) {
